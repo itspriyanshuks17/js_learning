e307824e9fbc2216f03a9affe28cb478bc96b0a3
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-06-05 07:00:33 +0530

    Consists of practice questions

---
+++ practice.js
@@ -0,0 +1,18 @@
+// 1. Create a const object called product to store info about the object
+const product = {
+  title: "ball pen",
+  rating: 4.5,
+  offer: 5,
+  price: 79
+}
+
+
+// 2. Create a const object called "profile" to store info about the person
+const profile = {
+  uname: "the_priyanshuk_sharma",
+  isFollow: true, 
+  followers: 230,
+  following: 340,
+  posts: 41,
+  bioDetails: "This ofiicial profile of Priyanshu Kumar Sharma"
+}
