5031fb56a11e2615e772e40381b9e4fc6046e3bc
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-06-05 06:08:00 +0530

    JS learning path

---
+++ first.js
@@ -0,0 +1,30 @@
+/*console.log("Welcome to Apna College");
+console.log("Priyanshu Kumar Sharma");*/
+/*JS: Dynamically typed language */ 
+
+/*Data Types and Variables*/
+
+/*age=25;
+name="Tony Stark"
+price=99.99;
+x=null;
+y=undefined;
+isFollow=false;
+console.log(name);
+console.log(age);
+console.log(x);
+console.log(price);
+console.log(y);
+console.log("Priyanshu K Sharma")*/
+
+/*Objects*/
+const student = {
+    name:"Priyanshu",
+    age:20,
+    cgpa: 10,
+    isPass: true
+};
+
+student['age'] = student['age'] + 1;
+
+console.log(student.age);
