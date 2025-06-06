94f6de6edf909bb5ad80fb9f5359d64d941affa2
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-06-06 11:31:39 +0530

    Ques for Loops & Strings added

--- practice.js
+++ practice.js
@@ -31,18 +31,50 @@
 
 // 2. Write a code which can give grades to students according to their scopes:
 
-let marks = prompt("Enter your marks:");
+// let marks = prompt("Enter your marks:");
 
-if(marks>=90) {
-  console.log("You got A");
-} else if(marks>=80 && marks<=89) {
-  console.log("You got B");
-} else if(marks>=70 && marks<=79) {
-  console.log("You got C");
-} else if(marks>=60 && marks<=69) {
-  console.log("You got D");
-} else if(marks>=50 && marks<=59) {
-  console.log("You got E");
-} else {
-  console.log("You failed");
-}
+// if(marks>=90) {
+//   console.log("You got A");
+// } else if(marks>=80 && marks<=89) {
+//   console.log("You got B");
+// } else if(marks>=70 && marks<=79) {
+//   console.log("You got C");
+// } else if(marks>=60 && marks<=69) {
+//   console.log("You got D");
+// } else if(marks>=50 && marks<=59) {
+//   console.log("You got E");
+// } else {
+//   console.log("You failed");
+// }
+
+// Loops and Strings
+// Loops
+// 1. Print all even numbers from 0 to 100
+// let num=0;
+// let n=100;
+
+// while(num<=n) {
+//   if(num%2===0) {
+//     console.log(num)
+//   }
+//   num++;
+// }
+
+// 2. Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
+// let gameNum = 25;
+
+// let userNum = prompt("Guess the game number: ");
+
+// while(userNum != gameNum) {
+//   userNum = prompt("Wrong number entered, enter again: ");
+// }
+
+// console.log("Numbers matched");
+
+
+// Strings
+// Prompt the userer to enter their full name. Generate a username for them based on the input. Start username with @, followed by their full name and ending with the fullname length
+let fullName=prompt("Enter your username: ");
+
+let userName = '@'+fullName+fullName.length;
+console.log(userName);
